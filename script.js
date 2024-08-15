207979fd225aaac8f06f4a2fb8ad9f74ee0c5a1b
Lingala Tharun Chandra <115937728+7337475780@users.noreply.github.com>
2024-08-15 12:28:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var nameError = document.getElementById("input-name");
+var phoneError = document.getElementById("phone-error");
+var mailError = document.getElementById("mail-error");
+var messageError = document.getElementById("message-error");
+var submitError = document.getElementById("submit-error");
+
+
+
